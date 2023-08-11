63e8c1f2bd4cd13a991251e6e43f4d849bcc92d4
khyeji98 <gpwl981207@naver.com>
2023-08-12 05:15:41 +0900

    feat: KakaoLoginHelper 정의 및 로그인 기능 구현

---
+++ FindOutCoffeePackage/Sources/LoginFeature/Dependency/KakaoLoginHelper.swift
@@ -0,0 +1,70 @@
+//
+//  KakaoLoginDelegate.swift
+//  
+//
+//  Created by 김혜지 on 2023/08/11.
+//
+
+import KakaoSDKAuth
+import KakaoSDKUser
+
+struct KakaoLoginHelper {
+    typealias UserCompletion = (User?, Error?) -> Void
+    
+    static func login(completion: @escaping UserCompletion) {
+        if AuthApi.hasToken() {
+            UserApi.shared.accessTokenInfo { _, error in
+                if error != nil {
+                    self.loginWithKakaoTalk { user, error in
+                        completion(user, error)
+                    }
+                } else {
+                    self.loadUserInfomation { user, error in
+                        completion(user, error)
+                    }
+                }
+            }
+        } else {
+            self.loginWithKakaoTalk { user, error in
+                completion(user, error)
+            }
+        }
+    }
+    
+    private static func loginWithKakaoTalk(completion: @escaping UserCompletion) {
+        if UserApi.isKakaoTalkLoginAvailable() {
+            self.loginWithApp { user, error in
+                completion(user, error)
+            }
+        } else {
+            self.loginWithWeb { user, error in
+                completion(user, error)
+            }
+        }
+    }
+    
+    private static func loginWithApp(completion: @escaping UserCompletion) {
+        UserApi.shared.loginWithKakaoTalk { _, error in
+            self.loadUserInfomation { user, error in
+                completion(user, error)
+            }
+        }
+    }
+    
+    private static func loginWithWeb(completion: @escaping UserCompletion) {
+        UserApi.shared.loginWithKakaoAccount { _, error in
+            self.loadUserInfomation { user, error in
+                completion(user, error)
+            }
+        }
+    }
+    
+    private static func loadUserInfomation(completion: @escaping UserCompletion) {
+        UserApi.shared.me { user, error in
+            let user: User? = User(id: user?.id?.description,
+                                   profileImageURL: user?.kakaoAccount?.profile?.profileImageUrl?.absoluteString,
+                                   nickname: user?.kakaoAccount?.profile?.nickname)
+            completion(user, error)
+        }
+    }
+}
