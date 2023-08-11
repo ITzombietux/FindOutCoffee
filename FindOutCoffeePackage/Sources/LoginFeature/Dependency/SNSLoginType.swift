f0ac2eff2ac4e699307c4d048c57f019e6131d56
khyeji98 <gpwl981207@naver.com>
2023-08-12 05:12:56 +0900

    feat: SNSLoginType 추가

---
+++ FindOutCoffeePackage/Sources/LoginFeature/Dependency/SNSLoginType.swift
@@ -0,0 +1,13 @@
+//
+//  SNSLoginType.swift
+//  
+//
+//  Created by 김혜지 on 2023/08/12.
+//
+
+extension AuthenticationClient {
+    enum SNSLoginType {
+        case apple
+        case kakao
+    }
+}
