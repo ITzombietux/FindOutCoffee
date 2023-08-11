355f87dc9df5ddcfa2772b2946ed79d62cb51283
khyeji98 <gpwl981207@naver.com>
2023-08-12 05:18:30 +0900

    feat: LoginCore 정의 및 액션 구현

--- FindOutCoffeePackage/Sources/LoginFeature/Core/LoginCore.swift
+++ FindOutCoffeePackage/Sources/LoginFeature/Core/LoginCore.swift
@@ -5,4 +5,52 @@
 //  Created by 김혜지 on 2023/08/08.
 //
 
-import Foundation
+import ComposableArchitecture
+
+public struct Login: Reducer {
+    public struct State: Equatable {
+        public init() {}
+    }
+    
+    public enum Action {
+        case apple
+        case kakao
+        case loginResponse(TaskResult<User>)
+    }
+    
+    @Dependency(\.authenticationClient) var authenticationClient
+    
+    public init() {}
+    
+    public func reduce(into state: inout State, action: Action) -> Effect<Action> {
+        switch action {
+        case .apple:
+            return .run { send in
+                do {
+                    let user = try await authenticationClient.login(.apple)
+                    await send(.loginResponse(.success(user)))
+                } catch {
+                    await send(.loginResponse(.failure(error)))
+                }
+            }
+            
+        case .kakao:
+            return .run { send in
+                do {
+                    let user = try await authenticationClient.login(.kakao)
+                    await send(.loginResponse(.success(user)))
+                } catch {
+                    await send(.loginResponse(.failure(error)))
+                }
+            }
+            
+        case let .loginResponse(.success(user)):
+            print("user", user)
+            return .none
+            
+        case let .loginResponse(.failure(error)):
+            print("error", error)
+            return .none
+        }
+    }
+}
