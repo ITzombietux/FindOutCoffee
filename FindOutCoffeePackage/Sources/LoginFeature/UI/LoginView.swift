ab03cb8e86b568359979416d71b9688d7ea2e8c5
khyeji98 <gpwl981207@naver.com>
2023-08-12 05:11:35 +0900

    feat: LoginView store 및 init 추가

--- FindOutCoffeePackage/Sources/LoginFeature/UI/LoginView.swift
+++ FindOutCoffeePackage/Sources/LoginFeature/UI/LoginView.swift
@@ -5,16 +5,34 @@
 //  Created by 김혜지 on 2023/08/09.
 //
 
+import ComposableArchitecture
+
 import SwiftUI
 
-struct LoginView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct LoginView: View {
+    private let store: StoreOf<Login>
+    
+    public init(store: StoreOf<Login> = Store(initialState: Login.State(), reducer: { Login() })) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack(spacing: 20) {
+                Button("애플 로그인") {
+                    viewStore.send(.apple)
+                }
+                
+                Button("카카오톡 로그인") {
+                    viewStore.send(.kakao)
+                }
+            }
+        }
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        LoginView(store: Store(initialState: Login.State(), reducer: { Login() }))
     }
 }
