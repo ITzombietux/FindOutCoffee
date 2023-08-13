d364ba91cd5937884a4c7224031291c6033f5322
khyeji98 <gpwl981207@naver.com>
2023-08-14 03:30:27 +0900

    feat: ReviewCore 구현

--- FindOutCoffeePackage/Sources/ReviewFeature/ReviewCore.swift
+++ FindOutCoffeePackage/Sources/ReviewFeature/ReviewCore.swift
@@ -5,4 +5,60 @@
 //  Created by 김혜지 on 2023/08/08.
 //
 
-import Foundation
+import ComposableArchitecture
+
+struct Review: Reducer {
+    enum Step: Int, Equatable {
+        case store = 1
+        case drink = 2
+        case options = 3
+        case price = 4
+        case writing = 5
+    }
+    
+    struct State: Equatable {
+        var step: Step
+        var content: ReviewContent.State
+        
+        init(step: Step = .store, content: ReviewContent.State = ReviewContent.State()) {
+            self.step = step
+            self.content = content
+        }
+    }
+    
+    enum Action {
+        case nextButtonTapped
+        case content(ReviewContent.Action)
+    }
+    
+    init() {}
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .nextButtonTapped:
+                switch state.step {
+                case .store:
+                    state.step = .drink
+                case .drink:
+                    state.step = .options
+                case .options:
+                    state.step = .price
+                case .price:
+                    state.step = .writing
+                case .writing:
+                    // TODO: 서버 통신
+                    break
+                }
+                return .none
+                
+            case .content:
+                return .none
+            }
+        }
+        
+        Scope(state: \.content, action: /Action.content) {
+            ReviewContent()
+        }
+    }
+}
