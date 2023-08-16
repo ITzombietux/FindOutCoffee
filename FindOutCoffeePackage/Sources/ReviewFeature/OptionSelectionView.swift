74e7c851bd53df7cdca6e0ea87aa2097c4503307
khyeji98 <gpwl981207@naver.com>
2023-08-17 00:54:21 +0900

    feat: OptionSelectionView UI 구현

--- FindOutCoffeePackage/Sources/ReviewFeature/OptionSelectionView.swift
+++ FindOutCoffeePackage/Sources/ReviewFeature/OptionSelectionView.swift
@@ -9,14 +9,33 @@ import SwiftUI
 
 extension ReviewContentView {
     struct OptionSelectionView: View {
+        @Binding var iceOrHot: ReviewContent.IceOrHot?
+        
+        init(iceOrHot: Binding<ReviewContent.IceOrHot?>) {
+            self._iceOrHot = iceOrHot
+        }
+        
         var body: some View {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack(alignment: .leading) {
+                Text("옵션을 선택해 주세요!")
+                    .font(.system(size: 25, weight: .bold))
+                
+                // TODO: 사이즈 항목
+                
+                SelectionCell(title: "❄️ ICE", isSelected: iceOrHot == .ice) {
+                    self.iceOrHot = .ice
+                }
+                
+                SelectionCell(title: "🔥 HOT", isSelected: iceOrHot == .hot) {
+                    self.iceOrHot = .hot
+                }
+            }
         }
     }
 }
 
 struct OptionSelectionView_Previews: PreviewProvider {
     static var previews: some View {
-        ReviewContentView.OptionSelectionView()
+        ReviewContentView.OptionSelectionView(iceOrHot: .constant(nil))
     }
 }
