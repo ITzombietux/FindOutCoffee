f737c609134d9d8d1dcef3dd8ab17ebaab99c6ad
MINSIK KIM <mansa.kim@kakaovx.com>
2023-08-21 12:54:38 +0900

    feat: MyView UI 추가

--- FindOutCoffeePackage/Sources/MyFeature/UI/MyView.swift
+++ FindOutCoffeePackage/Sources/MyFeature/UI/MyView.swift
@@ -9,30 +9,15 @@ import SwiftUI
 
 struct MyView: View {
     var body: some View {
-        HStack(alignment: .top) {
-            Text("만사만사만사")
-                .font(.system(size: 24, weight: .bold))
-                .foregroundColor(Color(red: 0.26, green: 0.26, blue: 0.26))
+        VStack(spacing: 60) {
+            ProfileView()
+                .padding(.vertical)
+
+            InformationView()
             
             Spacer()
             
-            AsyncImage(url: URL(string: "")!) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 72, height: 72)
-                    .clipped()
-            } placeholder: {
-                EmptyView()
-                    .background(.gray)
-            }
-            .cornerRadius(72)
-            .overlay(
-                RoundedRectangle(cornerRadius: 72)
-                    .inset(by: 0.25)
-                    .stroke(.black.opacity(0.1), lineWidth: 0.5)
-            )
-            .background(.gray)
+            LogoutView()
         }
         .padding()
     }
