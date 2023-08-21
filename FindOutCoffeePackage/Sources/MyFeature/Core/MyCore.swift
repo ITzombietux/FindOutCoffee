6b66e755f6fc0145a4805ded15424f7c3f677487
MINSIK KIM <mansa.kim@kakaovx.com>
2023-08-21 15:24:28 +0900

    feat: MyCore 추가

--- FindOutCoffeePackage/Sources/MyFeature/Core/MyCore.swift
+++ FindOutCoffeePackage/Sources/MyFeature/Core/MyCore.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+import ComposableArchitecture
+import UserDefaultsDependency
+import AuthorizationDependency
+
+public struct My: Reducer {
+    public struct State: Equatable {
+        public init() {}
+    }
+    
+    public enum Action {
+        case onAppear
+        case logoutButtonTapped
+        case withdrawalButtonTapped
+    }
+    
+    @Dependency(\.authenticationClient) var authenticationClient
+    @Dependency(\.loginClient) var loginClient
+    @Dependency(\.userDefaults) var userDefaultsClient
+    
+    public init() {}
+    
+    public func reduce(into state: inout State, action: Action) -> Effect<Action> {
+        switch action {
+        case .onAppear:
+            return .none
+            
+        case .logoutButtonTapped:
+            return .none
+            
+        case .withdrawalButtonTapped:
+            return .none
+        }
+    }
+}
