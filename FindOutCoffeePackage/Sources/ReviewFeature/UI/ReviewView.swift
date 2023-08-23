caee12f708bfb552f98a6a46212a195c3097aba3
Joy <joy.1207@kakaovx.com>
2023-08-23 15:23:10 +0900

    refactor: 네비게이션 버튼 이미지 리사이징

--- FindOutCoffeePackage/Sources/ReviewFeature/UI/ReviewView.swift
+++ FindOutCoffeePackage/Sources/ReviewFeature/UI/ReviewView.swift
@@ -47,7 +47,9 @@ public struct ReviewView: View {
                     viewStore.send(.backButtonTapped)
                 } label: {
                     Image(systemName: viewStore.state == 0 ? "multiply" : "chevron.left")
+                        .resizable()
                         .foregroundColor(Color.mainColor)
+                        .frame(width: 20, height: 20)
                 }
                 
                 Spacer()
