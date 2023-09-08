dbfdc2adb19263d2cfb3c76c3a2fed3d1057e4c4
Joy <joy.1207@kakaovx.com>
2023-09-08 14:31:24 +0900

    feat: ReviewDetailCore 추가

---
+++ FindOutCoffeePackage/Sources/ReviewDetailFeature/Core/ReviewDetailCore.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewDetailCore.swift
+//  
+//
+//  Created by Joy on 2023/09/08.
+//
+
+import ComposableArchitecture
+
+public struct ReviewDetail: Reducer {
+    public struct State: Equatable {
+        
+    }
+    
+    public enum Action {
+        
+    }
+    
+    public func reduce(into state: inout State, action: Action) -> Effect<Action> {
+        switch action {
+            
+        }
+    }
+}
